918a090cc8f780f99065b758b57f0a6436ba89df
AshleyBustos <ashley.bustos.e@gmail.com>
2022-11-02 12:58:11 -0300

    Se agregan botones y se está realizando el menu.json

--- burger-queen/src/pages/CustomerData.jsx
+++ burger-queen/src/pages/CustomerData.jsx
@@ -1,9 +1,17 @@
 import "./CustomerData.css";
+import { Link } from "react-router-dom"
 
 const CustomerData = () => {
   return (
     <div className="pageCustomerData">
-      <img src="machineFlint.png" alt="machine-Flint" />
+      <img id="machine-Flint" src="machineFlint.png" alt="machine-Flint" />
+      <div className="customerInfo">
+        <h3>Nombre:</h3>
+        <input type="text" class="input" placeholder="Nombre"></input>
+        <h3>Mesa:</h3>
+        <input type="number" class="input" placeholder="Mesa"></input>
+        <button><Link to="/CustomerOrder">Ir a pedido</Link></button>
+      </div>
     </div>
   );
 };
--- burger-queen/src/pages/Who.jsx
+++ burger-queen/src/pages/Who.jsx
@@ -1,12 +1,16 @@
 import "./Who.css";
+import { Link } from "react-router-dom";
 
 const Who = () => {
   return (
     <div className="pageWho">
       <div className="work-income">
         <h1>¿Quién eres?</h1>
+        <button><Link to="/CustomerData">Mesero</Link></button>
+        <button><Link to="/Kitchen">Jefe de cocina</Link></button>
       </div>
       <div className="imgFlintLoco">
+        <h1>FLINT LOCO BURGERS</h1>
         <img src="flintLoco.png" alt="flint-saluda" />
       </div>
     </div>
