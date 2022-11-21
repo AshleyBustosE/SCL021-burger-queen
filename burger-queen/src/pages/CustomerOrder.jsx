9b17df443a45f036805c0a0b2bbca45adc665d12
AshleyBustos <ashley.bustos.e@gmail.com>
2022-11-21 12:41:03 -0300

    Se muestra en el muro en forma de lista los items del menu

--- burger-queen/src/pages/CustomerOrder.jsx
+++ burger-queen/src/pages/CustomerOrder.jsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import menu from "../data/menu.json";
+import { useState } from "react";
 
 import "./CustomerOrder.css";
 
@@ -7,13 +8,73 @@ const breakfast = menu.breakfast;
 const lunch = menu.lunch;
 console.log(breakfast);
 console.log(lunch);
+
+function MyButton() {
+  const [count, setCount] = useState(0);
+  
+  function handleClick() {
+    setCount(count + 1);
+  }
+  
+    return (
+      <button onClick={handleClick}>
+        Clicked {count} times
+      </button>
+    );
+}
+
+function ListBreakfast() {
+   const listItemsBreakfast = breakfast.map((productBreakfast) => 
+     <li key={productBreakfast.id}>
+        {productBreakfast.item}
+     </li>
+   );
+  return (
+  <ul>{listItemsBreakfast}</ul>
+  );
+}
+
+function ListLunch() {
+  const listItemsLunch = lunch.map((productLunch) => 
+    <li key={productLunch.id}>
+       {productLunch.item}
+    </li>
+  );
+ return (
+ <ul>{listItemsLunch}</ul>
+ );
+}
+
+// export default function MyApp() {
+//   return (
+//     <div>
+//       <h1>Counters that update separately</h1>
+//       <MyButton />
+//       <MyButton />
+//       <ShoppingList />
+//     </div>
+//   );
+// }
+
 const CustomerOrder = () => {
   //console.log(ListMenu);
   return (
     <div className="pageCustomerOrder">
-      <div>
+      <div className="customerData">
+        <p>Nombre:</p>
+        <input type="text" placeholder="Nombre cliente" />
+        <p>Mesa:</p>
+        <input type="number" placeholder="Mesa" />
       </div>
-      <div></div>
+      <div className="dailyMenu">
+        <div className="breakfastMenu">
+          <MyButton />
+          <MyButton />
+          <ListBreakfast />
+          <ListLunch />
+        </div>
+        <div className="lunchMenu"></div>
+      </div>
       <div className="waiter">
         <div className="orderList">
           <h3>Orden</h3>
